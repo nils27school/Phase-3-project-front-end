ccddd38a4096e3a908fae15ca705006bfaf66fbe
nilssvensson <nils27school@gmail.com>
2022-11-15 12:51:17 -0500

    assets folder

---
+++ project-frontend/src/Assets.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// cocktails
+<div>
+{/* Old Fashion  */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.simplyrecipes.com%2Fthmb%2Fs_de1Nuw4ULiHNECVHOCBY5u5Wk%3D%2F1500x0%2Ffilters%3Ano_upscale()%3Amax_bytes(150000)%3Astrip_icc()%2F__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2020__01__Old-Fashioned-Cocktail-LEAD-5-1024x681-aa81a798a156453d80d1f7d41de893ff.jpg&imgrefurl=https%3A%2F%2Fwww.simplyrecipes.com%2Frecipes%2Fold_fashioned_cocktail%2F&tbnid=ShtMOn1FOxlBfM&vet=12ahUKEwjPk6Cx1bD7AhV8qHIEHd2JCjkQMygDegUIARCKAw..i&docid=oa5lckOJdW_zOM&w=1024&h=681&q=old%20fashion&ved=2ahUKEwjPk6Cx1bD7AhV8qHIEHd2JCjkQMygDegUIARCKAw" alt="Old Fashion"/>
+{/* Margarita  */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fbakeitwithlove.com%2Fwp-content%2Fuploads%2F2021%2F09%2FVodka-Margarita-sq.jpg&imgrefurl=https%3A%2F%2Fbakeitwithlove.com%2Fvodka-margarita%2F&tbnid=kNdJEm-oyZR--M&vet=12ahUKEwin343R1bD7AhW2n3IEHSiSAKkQMygIegUIARClAw..i&docid=ZMn6vlvAgjFGhM&w=1200&h=1200&q=margarita&ved=2ahUKEwin343R1bD7AhW2n3IEHSiSAKkQMygIegUIARClAw" alt="Margarita"/>
+{/* Cosmo  */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.simplejoy.com%2Fwp-content%2Fuploads%2F2018%2F03%2Fcosmopolitan_drink-683x1024.jpg&imgrefurl=https%3A%2F%2Fwww.simplejoy.com%2Fcosmopolitan-cocktail%2F&tbnid=YmgQS7jHMBD79M&vet=12ahUKEwim-6-W1rD7AhVSvHIEHeQMDb8QMygKegUIARDlAQ..i&docid=ZNKf2ccYZXVYZM&w=683&h=1024&q=cosmo%20ingredients&ved=2ahUKEwim-6-W1rD7AhVSvHIEHeQMDb8QMygKegUIARDlAQ" alt="Cosmo"/>
+
+// spirits
+{/* Bourbon */}
+ <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Ficdn.bottlenose.wine%2Fimages%2Ffull%2F425184.jpg%3Fmin-w%3D200%26min-h%3D200%26fit%3Dcrop&imgrefurl=https%3A%2F%2Fwww.martinbruniliquor.com%2Fproducts%2F10160390%2Fbulleit-frontier-bourbon-whiskey&tbnid=Vnqy6kJUxq9haM&vet=12ahUKEwjHwOa81rD7AhV9EFkFHWG-C_IQMygTegUIARDBBA..i&docid=15w_eKv-ilLISM&w=521&h=1300&q=bourbon&ved=2ahUKEwjHwOa81rD7AhV9EFkFHWG-C_IQMygTegUIARDBBA" alt="Bourbon"/>
+{/* Vodka  */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.absolut.com%2Fglobalassets%2Fimages%2Fproducts%2Fabsolut-vodka%2Fatlas%2Fingredient_absolut-vodka_1000ml_us_packshot.jpg&imgrefurl=https%3A%2F%2Fwww.absolut.com%2Fus%2Fproducts%2F&tbnid=dqRH-8Q_91cDNM&vet=12ahUKEwiRlrLT1rD7AhXiEVkFHTm9DOoQMygBegUIARC4Aw..i&docid=jKEK37GAywH9_M&w=1080&h=1840&q=vodka&ved=2ahUKEwiRlrLT1rD7AhXiEVkFHTm9DOoQMygBegUIARC4Aw" alt="Vodka" />
+
+// glassware
+{/* Rocks Glass  */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.shoplightspeed.com%2Fshops%2F606142%2Ffiles%2F15047225%2F1500x4000x3%2Fcocktail-kingdom-yarai-rocks-glass-75oz.jpg&imgrefurl=https%3A%2F%2Fwww.thebostonshaker.com%2Fyarai-rocks-glass-75oz.html&tbnid=3strJuW2DepcsM&vet=12ahUKEwiT3rLx1rD7AhXPGVkFHWsNC34QMygDegUIARC9Aw..i&docid=5rQZn1msxO9jaM&w=1500&h=1500&q=rocks%20glass&ved=2ahUKEwiT3rLx1rD7AhXPGVkFHWsNC34QMygDegUIARC9Aw" alt="Rocks Glass" />
+{/* Coupe  */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdnimg.webstaurantstore.com%2Fimages%2Fproducts%2Flarge%2F275563%2F2261576.jpg&imgrefurl=https%3A%2F%2Fwww.webstaurantstore.com%2Facopa-8-5-oz-coupe-cocktail-glass-case%2F5535441.html&tbnid=hoOtKEoeCVhp7M&vet=12ahUKEwjiq56G17D7AhV-FlkFHZFCAJMQMygCegUIARCIAw..i&docid=tdajsKTOo1AQ-M&w=600&h=600&q=coupe%20glass&ved=2ahUKEwjiq56G17D7AhV-FlkFHZFCAJMQMygCegUIARCIAw" alt="Coupe"/>
+</div>
+
