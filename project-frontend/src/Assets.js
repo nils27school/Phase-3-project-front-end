67556864591c2f64ea0b6dd27c7e660fe70e8ffc
nilssvensson <nils27school@gmail.com>
2022-11-16 13:44:40 -0500

    image update

--- project-frontend/src/Assets.js
+++ project-frontend/src/Assets.js
@@ -8,17 +8,38 @@ import React from 'react';
 <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fbakeitwithlove.com%2Fwp-content%2Fuploads%2F2021%2F09%2FVodka-Margarita-sq.jpg&imgrefurl=https%3A%2F%2Fbakeitwithlove.com%2Fvodka-margarita%2F&tbnid=kNdJEm-oyZR--M&vet=12ahUKEwin343R1bD7AhW2n3IEHSiSAKkQMygIegUIARClAw..i&docid=ZMn6vlvAgjFGhM&w=1200&h=1200&q=margarita&ved=2ahUKEwin343R1bD7AhW2n3IEHSiSAKkQMygIegUIARClAw" alt="Margarita"/>
 {/* Cosmo  */}
 <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.simplejoy.com%2Fwp-content%2Fuploads%2F2018%2F03%2Fcosmopolitan_drink-683x1024.jpg&imgrefurl=https%3A%2F%2Fwww.simplejoy.com%2Fcosmopolitan-cocktail%2F&tbnid=YmgQS7jHMBD79M&vet=12ahUKEwim-6-W1rD7AhVSvHIEHeQMDb8QMygKegUIARDlAQ..i&docid=ZNKf2ccYZXVYZM&w=683&h=1024&q=cosmo%20ingredients&ved=2ahUKEwim-6-W1rD7AhVSvHIEHeQMDb8QMygKegUIARDlAQ" alt="Cosmo"/>
+{/* Whiskey Sour */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fassets.bonappetit.com%2Fphotos%2F57acc14e53e63daf11a4d9b6%2F1%3A1%2Fw_2560%252Cc_limit%2Fwhiskey-sour.jpg&imgrefurl=https%3A%2F%2Fwww.bonappetit.com%2Frecipe%2Fwhiskey-sour&tbnid=tEACfBpFqW8EFM&vet=12ahUKEwiQuYqnlrP7AhUprHIEHbDpDLoQMygBegUIARCLAg..i&docid=TTQv-rgUvDBn6M&w=2069&h=2069&q=whiskey%20sour&ved=2ahUKEwiQuYqnlrP7AhUprHIEHbDpDLoQMygBegUIARCLAg" alt="Whiskey Sour" />
+{/* Mint Julep */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages-gmi-pmc.edge-generalmills.com%2Fd3191d15-728b-400a-a475-45509d3c471f.jpg&imgrefurl=https%3A%2F%2Fwww.bettycrocker.com%2Frecipes%2Ftraditional-mint-julep%2F2dc83b52-49e5-436f-be93-02834f07c08f&tbnid=GLh8UztwHGxC9M&vet=12ahUKEwiVktS6lrP7AhXErHIEHaD9Ca8QMygTegUIARC7Ag..i&docid=P4wTX6um9LRywM&w=800&h=450&q=mint%20julep&ved=2ahUKEwiVktS6lrP7AhXErHIEHaD9Ca8QMygTegUIARC7Ag" alt="Mint Julep" />
 
-// spirits
+
+{/* //spirits */}
 {/* Bourbon */}
- <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Ficdn.bottlenose.wine%2Fimages%2Ffull%2F425184.jpg%3Fmin-w%3D200%26min-h%3D200%26fit%3Dcrop&imgrefurl=https%3A%2F%2Fwww.martinbruniliquor.com%2Fproducts%2F10160390%2Fbulleit-frontier-bourbon-whiskey&tbnid=Vnqy6kJUxq9haM&vet=12ahUKEwjHwOa81rD7AhV9EFkFHWG-C_IQMygTegUIARDBBA..i&docid=15w_eKv-ilLISM&w=521&h=1300&q=bourbon&ved=2ahUKEwjHwOa81rD7AhV9EFkFHWG-C_IQMygTegUIARDBBA" alt="Bourbon"/>
+ <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Ficdn.bottlenose.wine%2Fimages%2Ffull%2F425184.jpg%3Fmin-w%3D200%26min-h%3D200%26fit%3Dcrop&imgrefurl=https%3A%2F%2Fwww.martinbruniliquor.com%2Fproducts%2F10160390%2Fbulleit-frontier-bourbon-whiskey&tbnid=Vnqy6kJUxq9haM&vet=12ahUKEwjHwOa81rD7AhV9EFkFHWG-C_IQMygTegUIARDBBA..i&docid=15w_eKv-ilLISM&w=521&h=1300&q=bourbon&ved=2ahUKEwjHwOa81rD7AhV9EFkFHWG-C_IQMygTegUIARDBBA" alt="Whiskey"/>
 {/* Vodka  */}
 <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.absolut.com%2Fglobalassets%2Fimages%2Fproducts%2Fabsolut-vodka%2Fatlas%2Fingredient_absolut-vodka_1000ml_us_packshot.jpg&imgrefurl=https%3A%2F%2Fwww.absolut.com%2Fus%2Fproducts%2F&tbnid=dqRH-8Q_91cDNM&vet=12ahUKEwiRlrLT1rD7AhXiEVkFHTm9DOoQMygBegUIARC4Aw..i&docid=jKEK37GAywH9_M&w=1080&h=1840&q=vodka&ved=2ahUKEwiRlrLT1rD7AhXiEVkFHTm9DOoQMygBegUIARC4Aw" alt="Vodka" />
+{/* Tequila */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.totalwine.com%2Fdynamic%2F490x%2Fmedia%2Fsys_master%2Ftwmmedia%2Fh24%2Fh94%2F10940880879646.png&imgrefurl=https%3A%2F%2Fwww.totalwine.com%2Fspirits%2Ftequila%2Fblancosilver%2Fcasamigos-blanco%2Fp%2F130475750&tbnid=XoFnjYynFUmh4M&vet=12ahUKEwj-yorolLP7AhWcM1kFHXU-C0UQMygAegUIARCFAw..i&docid=0DL5issRSQCqqM&w=490&h=686&q=tequila%20casamigos&ved=2ahUKEwj-yorolLP7AhWcM1kFHXU-C0UQMygAegUIARCFAw" alt="Tequila" />
+{/* Rum */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.specsonline.com%2Fwp-content%2Fuploads%2F2021%2F11%2F008048002520.jpg&imgrefurl=https%3A%2F%2Fspecsonline.com%2Fshop%2Fspirits%2Fbacardi-rum-gold%2F&tbnid=TyFgfnsAjDjZZM&vet=12ahUKEwjO8On5lLP7AhVSGVkFHTrrCXYQMygAegUIARCUBA..i&docid=dIshmhxkTukGnM&w=1000&h=1000&q=rum&ved=2ahUKEwjO8On5lLP7AhVSGVkFHTrrCXYQMygAegUIARCUBA" alt="Rum" />
+{/* Brandy */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0099%2F0045%2F8042%2Fproducts%2FStudio_Project-2022-05-31T082550.317_59f1bd08-16c6-42b4-a45e-8dbefef57245.jpg%3Fv%3D1654010790&imgrefurl=https%3A%2F%2Fshopsk.com%2Fproducts%2Fstella-rosa-brandy-tropical-passion-750ml&tbnid=hsWG7UOJx0qF6M&vet=12ahUKEwjCxbmSlbP7AhVGFlkFHbBSAu4Q94IIKB96BQgBEMMC..i&docid=P-POVt_E78fAtM&w=1806&h=1806&q=brandy&ved=2ahUKEwjCxbmSlbP7AhVGFlkFHbBSAu4Q94IIKB96BQgBEMMC" alt="Brandy" />
+{/* Gin */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fproducts2.imgix.drizly.com%2Fci-tanqueray-london-dry-gin-e2ee842638459d4c.jpeg%3Fauto%3Dformat%252Ccompress%26ch%3DWidth%252CDPR%26fm%3Djpg%26q%3D20&imgrefurl=https%3A%2F%2Fdrizly.com%2Fliquor%2Fgin%2Flondon-dry-gin%2Ftanqueray-london-dry-gin-946-proof%2Fp146&tbnid=yr3gHBpFNkYHoM&vet=12ahUKEwjop6-rlbP7AhUpFlkFHULOCWgQ94IIKAB6BQgBEJsE..i&docid=cwPlhQFsuKsuQM&w=2000&h=2000&q=gin&ved=2ahUKEwjop6-rlbP7AhUpFlkFHULOCWgQ94IIKAB6BQgBEJsE" alt="Gin" />
 
-// glassware
+{/* // glassware */}
 {/* Rocks Glass  */}
 <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.shoplightspeed.com%2Fshops%2F606142%2Ffiles%2F15047225%2F1500x4000x3%2Fcocktail-kingdom-yarai-rocks-glass-75oz.jpg&imgrefurl=https%3A%2F%2Fwww.thebostonshaker.com%2Fyarai-rocks-glass-75oz.html&tbnid=3strJuW2DepcsM&vet=12ahUKEwiT3rLx1rD7AhXPGVkFHWsNC34QMygDegUIARC9Aw..i&docid=5rQZn1msxO9jaM&w=1500&h=1500&q=rocks%20glass&ved=2ahUKEwiT3rLx1rD7AhXPGVkFHWsNC34QMygDegUIARC9Aw" alt="Rocks Glass" />
 {/* Coupe  */}
 <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdnimg.webstaurantstore.com%2Fimages%2Fproducts%2Flarge%2F275563%2F2261576.jpg&imgrefurl=https%3A%2F%2Fwww.webstaurantstore.com%2Facopa-8-5-oz-coupe-cocktail-glass-case%2F5535441.html&tbnid=hoOtKEoeCVhp7M&vet=12ahUKEwjiq56G17D7AhV-FlkFHZFCAJMQMygCegUIARCIAw..i&docid=tdajsKTOo1AQ-M&w=600&h=600&q=coupe%20glass&ved=2ahUKEwjiq56G17D7AhV-FlkFHZFCAJMQMygCegUIARCIAw" alt="Coupe"/>
+{/* Collins Glass */}
+<img src= "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.webstaurantstore.com%2Fimages%2Fproducts%2Flarge%2F583007%2F2267686.jpg&imgrefurl=https%3A%2F%2Fwww.webstaurantstore.com%2Fchoice-13-oz-san-plastic-tom-collins-glass-case%2F176BWSCLLN14.html&tbnid=ae1mVi5GR3JnjM&vet=12ahUKEwjky7HClbP7AhWqn3IEHXtWAYwQMygCegUIARDvAg..i&docid=ladsf3cxrXQinM&w=600&h=600&q=collins%20glass&ved=2ahUKEwjky7HClbP7AhWqn3IEHXtWAYwQMygCegUIARDvAg" alt="Collins Glass" />
+{/* Highball Glass */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.restaurantware.com%2Fmedia%2Fcatalog%2Fproduct%2FR%2FW%2FRWG0099-1-LR.jpg%3Foptimize%3Dhigh%26fit%3Dbounds%26height%3D700%26width%3D700&imgrefurl=https%3A%2F%2Fwww.restaurantware.com%2Ftableware%2Fdrinkware%2Fcocktail-glasses%2Fotto-10-oz-highball-glass-3-x-3-x-4-3-4-6-count-box%2F&tbnid=XmRmL280152B4M&vet=12ahUKEwiRnubTlbP7AhXPrXIEHTLYC1wQMygKegUIARCXAw..i&docid=Z661a4wcBAhb0M&w=700&h=700&q=highball%20glass&ved=2ahUKEwiRnubTlbP7AhXPrXIEHTLYC1wQMygKegUIARCXAw" alt="Highball Glass" />
+{/* Shot Glass */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.totallypromotional.com%2Fvarnish-images%2FData%2FMedia%2FCatalog%2F6%2F800%2F457a0398-c5f9-4147-a69e-7b2ca02542aa2-oz-Deluxe-Tapered-Acrylic-Shot-Glass-Full-Color-H176F-clear.jpg&imgrefurl=https%3A%2F%2Fwww.totallypromotional.com%2F2oz-deluxe-tapered-acrylic-shot-glass-full-color.html&tbnid=cFR-X8HDzSwD5M&vet=12ahUKEwijjaTrlbP7AhWon3IEHdcGDMsQMygDegUIARDNAw..i&docid=KNIbbuAq9Hkm7M&w=800&h=800&q=shot%20glass&ved=2ahUKEwijjaTrlbP7AhWon3IEHdcGDMsQMygDegUIARDNAw" alt="Shot Glass" />
+{/* Martini Glass */}
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.arc-online.com%2Fen-us%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F394f734a6220a50fe0c060b78a87b444%2Fu%2Fs%2FUS_Atlas_Martini.jpg&imgrefurl=https%3A%2F%2Fwww.arc-online.com%2Fen-us%2Fatlas-martini-glass-7-5-oz-4pc-set&tbnid=L5r7kJO6ZKQiFM&vet=12ahUKEwjYzOb8lbP7AhVCn3IEHQm2A6sQMygCegUIARDhAw..i&docid=eeKHXcxUZANX6M&w=3050&h=3050&q=martini%20glass&ved=2ahUKEwjYzOb8lbP7AhVCn3IEHQm2A6sQMygCegUIARDhAw" alt="Martini Glass" />
 </div>
 
